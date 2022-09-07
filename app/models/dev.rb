b6a738e352bc7fea64de9974249a10bed06b52c2
Dana Lobell <danalobell@gmail.com>
2022-09-07 11:27:35 -0400

    almost done

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,16 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        
+    end
+
+        # accepts an item_name (string) and returns true if any of the 
+        # freebies associated with the dev has that item_name, otherwise 
+        # returns false
+    def give_away(dev, freebie)
+
+    end 
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+company_one = Company.create(name: "Google", founding_year: 1998)
+company_two = Company.create(name: "Facebook", founding_year: 2004)
+company_three = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+company_four = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+dev_one = Dev.create(name: "Rick")
+dev_two = Dev.create(name: "Morty")
+dev_three = Dev.create(name: "Mr. Meseeks")
+dev_four = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -17,5 +17,9 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+Freebie.create(item_name: "keychain", value: 33, company_id: company_one.id, dev_id: dev_one.id)
+Freebie.create(item_name: "toothbrush", value: 57, company_id: company_two.id, dev_id: dev_two.id)
+Freebie.create(item_name: "magnet", value: 45, company_id: company_three.id, dev_id: dev_three.id)
+Freebie.create(item_name: "pen", value: 17, company_id: company_four.id, dev_id: dev_four.id)
 
 puts "Seeding done!"
